b5e9a3ab2bb1c962dc631b6606c7c9e4b2e41214
Tim Grannen <grannendev@gmail.com>
2022-12-01 17:36:56 -0500

    Rework DayOne solution to use SplitOn extension method

--- AdventOfCode/AdventOfCode/01/DayOne.cs
+++ AdventOfCode/AdventOfCode/01/DayOne.cs
@@ -6,7 +6,10 @@ public class DayOne
 
     public DayOne(IEnumerable<string> lines)
     {
-        _elfCalories = GetElfCalories(lines);
+        _elfCalories =
+            lines.SplitOn(string.IsNullOrEmpty)
+                .Select(elf => elf.Select(int.Parse).Sum())
+                .ToList();
     }
 
     public int PartOne()
@@ -17,26 +20,5 @@ public class DayOne
     public int PartTwo()
     {
         return _elfCalories.OrderByDescending(x => x).Take(3).Sum();
-    }
-
-    private static List<int> GetElfCalories(IEnumerable<string> lines)
-    {
-        var calories = new List<int>();
-        var elfCalories = new List<int>();
-        foreach (var line in lines)
-        {
-            if (int.TryParse(line, out var cals))
-            {
-                calories.Add(cals);
-            }
-            else
-            {
-                elfCalories.Add(calories.Sum());
-                calories.Clear();
-            }
-        }
-
-        elfCalories.Add(calories.Sum());
-        return elfCalories;
     }
 }
---
+++ AdventOfCode/AdventOfCode/Shared/Extensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Shared;
+
+public static class Extensions
+{
+    public static IEnumerable<List<T>> SplitOn<T>(this IEnumerable<T> objects, Func<T, bool> predicate)
+    {
+        var currentItems = new List<T>();
+        foreach (var item in objects)
+        {
+            if (!predicate(item))
+            {
+                currentItems.Add(item);
+                continue;
+            }
+
+            yield return currentItems;
+            currentItems = new List<T>();
+        }
+
+        yield return currentItems;
+    }
+}
