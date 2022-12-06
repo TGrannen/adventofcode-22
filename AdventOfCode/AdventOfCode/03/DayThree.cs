88200a1cf1f1fdb800734f7d579115980f514df1
Tim Grannen <grannendev@gmail.com>
2022-12-06 01:28:04 -0500

    Refactor Day Three

--- AdventOfCode/AdventOfCode/03/DayThree.cs
+++ AdventOfCode/AdventOfCode/03/DayThree.cs
@@ -1,61 +1,27 @@
-﻿using System.Diagnostics.SymbolStore;
-
-namespace AdventOfCode._03;
+﻿namespace AdventOfCode._03;
 
 public class DayThree
 {
-    private readonly List<Parts> _parts;
-    private readonly List<Slice> _slicedParts;
+    private readonly int[] _parts;
+    private readonly int[] _slicedParts;
 
     public DayThree(IEnumerable<string> lines)
     {
         _parts = lines.Select(x =>
         {
             var split = x.Insert(x.Length / 2, "|").Split(new[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
-            return new Parts
-            {
-                OriginalDistinct = string.Join("", x.Distinct()),
-                Part1 = split[0],
-                Part2 = split[1]
-            };
-        }).ToList();
+            return split[0].AsEnumerable().Intersect(split[1]).Distinct().ToList().Select(CharScore).Sum();
+        }).ToArray();
 
-        _slicedParts = _parts.Slice(3).Select(x => new Slice { Items = x.ToArray() }).ToList();
+        var strings = lines.Select(x => new string(x.Distinct().ToArray())).Slice(3).Select(x => string.Join("", x)).ToArray();
+        var chars = strings.Select(x => x.GroupBy(y => y).First(y => y.Count() == 3).Key).ToArray();
+
+        _slicedParts = chars.Select(CharScore).ToArray();
     }
 
-    public int PartOne()
-    {
-        return _parts.Sum(x =>
-        {
-            var chars = x.Part1.AsEnumerable().Intersect(x.Part2).Distinct().ToList();
-            return chars.Select(Parts.CharScore).Sum();
-        });
-    }
+    public int PartOne() => _parts.Sum();
 
-    public int PartTwo()
-    {
-        return _slicedParts.Select(x => Parts.CharScore(x.TopChar())).Sum();
-    }
+    public int PartTwo() => _slicedParts.Sum();
 
-    private class Parts
-    {
-        public string OriginalDistinct { get; init; }
-        public string Part1 { get; init; }
-        public string Part2 { get; init; }
-
-        public static int CharScore(char x) => x <= 'a' ? x - 'A' + 27 : x - 'a' + 1;
-    }
-
-    private class Slice
-    {
-        public Parts[] Items { get; init; }
-
-        public char TopChar()
-        {
-            return 
-                string.Join("", Items.Select(x => x.OriginalDistinct))
-                .GroupBy(x => x)
-                .First(x => x.Count() == 3).Key;
-        }
-    }
+    private static int CharScore(char x) => x <= 'a' ? x - 'A' + 27 : x - 'a' + 1;
 }
