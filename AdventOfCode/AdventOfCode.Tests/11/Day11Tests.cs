c2b0c862d6434b3f7223aaeae5fafb696c80f2e0
Tim Grannen <grannendev@gmail.com>
2022-12-12 00:09:22 -0500

    Day 11 Solution

--- AdventOfCode/AdventOfCode.Tests/11/Day11Tests.cs
+++ AdventOfCode/AdventOfCode.Tests/11/Day11Tests.cs
@@ -1,38 +1,60 @@
-﻿namespace AdventOfCode.Tests._11;
+﻿using AdventOfCode._11;
 
+namespace AdventOfCode.Tests._11;
+
+[UsesVerify]
 public class Day11Tests
 {
     private const int Day = 11;
 
-    [Fact(Skip = "Problem not out yet")]
+    [Fact]
     public async Task PartOne_ExampleTest()
     {
         var contents = await FileIOWrapper.ReadAllLinesAsync(Day, @"Input\Example.txt");
-        var result = new TempClass(contents).PartOne();
-        result.Should().Be(0);
+        var dayEleven = new DayEleven(contents);
+        dayEleven.Process(20, true);
+        dayEleven.GetMonkeyBusiness().Should().Be(10605);
     }
 
-    [Fact(Skip = "Problem not out yet")]
+    [Fact]
     public async Task PartOne_ProblemTest()
     {
         var contents = await FileIOWrapper.ReadAllLinesAsync(Day, @"Input\Problem.txt");
-        var result = new TempClass(contents).PartOne();
-        result.Should().Be(0);
+        var dayEleven = new DayEleven(contents);
+        dayEleven.Process(20, true);
+        dayEleven.GetMonkeyBusiness().Should().Be(57348);
     }
 
-    [Fact(Skip = "Problem not out yet")]
+    [Fact]
     public async Task PartTwo_ExampleTest()
     {
         var contents = await FileIOWrapper.ReadAllLinesAsync(Day, @"Input\Example.txt");
-        var result = new TempClass(contents).PartTwo();
-        result.Should().Be(0);
+        var dayEleven = new DayEleven(contents);
+        dayEleven.Process(10_000, false);
+        dayEleven.GetMonkeyBusiness().Should().Be(2713310158);
     }
 
-    [Fact(Skip = "Problem not out yet")]
+    [Fact]
     public async Task PartTwo_ProblemTest()
     {
         var contents = await FileIOWrapper.ReadAllLinesAsync(Day, @"Input\Problem.txt");
-        var result = new TempClass(contents).PartTwo();
-        result.Should().Be(0);
+        var dayEleven = new DayEleven(contents);
+        dayEleven.Process(10_000, false);
+        dayEleven.GetMonkeyBusiness().Should().Be(14106266886);
+    }
+
+    [Fact]
+    public async Task Monkeys_ShouldHaveProcessCounts_AfterRounds()
+    {
+        var contents = await FileIOWrapper.ReadAllLinesAsync(Day, @"Input\Example.txt");
+
+        await Verify(new
+        {
+            _1 = new DayEleven(contents).Process(1, false).Select(x => $"{x.Number} - {x.ProcessCount}"),
+            _20 = new DayEleven(contents).Process(20, false).Select(x => $"{x.Number} - {x.ProcessCount}"),
+            _1000 = new DayEleven(contents).Process(1000, false).Select(x => $"{x.Number} - {x.ProcessCount}"),
+            _5000 = new DayEleven(contents).Process(5_000, false).Select(x => $"{x.Number} - {x.ProcessCount}"),
+            _10000 = new DayEleven(contents).Process(10_000, false).Select(x => $"{x.Number} - {x.ProcessCount}"),
+        }).UseDirectory("Snapshots");
     }
 }
---
+++ AdventOfCode/AdventOfCode/11/DayEleven.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode._11;
+
+public class DayEleven
+{
+    private readonly Monkey[] _monkeys;
+
+    public DayEleven(IEnumerable<string> contents)
+    {
+        _monkeys = contents.SplitOn(string.IsNullOrEmpty).Select(x => new Monkey(x.ToArray())).ToArray();
+    }
+
+    public long GetMonkeyBusiness()
+    {
+        var values = _monkeys.OrderByDescending(x => x.ProcessCount).Select(x => x.ProcessCount).Take(2).ToArray();
+        return values[0] * values[1];
+    }
+
+    public Monkey[] Process(int numberOfRounds, bool divideByThree)
+    {
+        var productOfDivisors = _monkeys.Select(x => x.Divisor).Aggregate(1, (x, y) => x * y);
+        Func<long, long> managementFunc = divideByThree ? l => l / 3 : l => l % productOfDivisors;
+
+        for (var i = 0; i < numberOfRounds; i++)
+        {
+            foreach (var monkey in _monkeys.Where(x => x.Items.Any()))
+            {
+                monkey.Process(_monkeys, managementFunc);
+            }
+        }
+
+        return _monkeys;
+    }
+}
+
+public class Monkey
+{
+    public Monkey(string[] details)
+    {
+        Number = int.Parse(details[0].Replace("Monkey ", "").Replace(":", ""));
+        foreach (var num in details[1].Replace("Starting items: ", "").Split(",").Select(int.Parse).ToList())
+        {
+            Items.Enqueue(num);
+        }
+
+        var split = details[2].Replace("  Operation: new = ", "").Split(" ").ToArray();
+        _first = split[0];
+        _second = split[2];
+        _operatorSymbol = split[1];
+        Divisor = int.Parse(details[3][21..]);
+        var trueNumber = int.Parse(details[4][29..]);
+        var falseNumber = int.Parse(details[5][30..]);
+        ToThrowTo = item => item % Divisor == 0 ? trueNumber : falseNumber;
+    }
+
+    public int Number { get; }
+    public int Divisor { get; }
+    public Queue<long> Items { get; } = new();
+    private Func<long, int> ToThrowTo { get; }
+    public long ProcessCount { get; private set; }
+
+    private readonly string _first;
+    private readonly string _second;
+    private readonly string _operatorSymbol;
+
+    public void Process(Monkey[] monkeys, Func<long, long> managementFunc)
+    {
+        while (Items.Any())
+        {
+            var item = Items.Dequeue();
+            var first = _first == "old" ? item : long.Parse(_first);
+            var second = _second == "old" ? item : long.Parse(_second);
+            var newLevel = _operatorSymbol switch
+            {
+                "+" => first + second,
+                "-" => first - second,
+                "*" => first * second,
+                "/" => first / second
+            };
+            item = managementFunc(newLevel);
+            monkeys[ToThrowTo(item)].Items.Enqueue(item);
+            ProcessCount++;
+        }
+    }
+}
